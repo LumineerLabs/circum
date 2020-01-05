b4e4097a290589119d84c3d74a1f7a7db13f7b29
Lane Haury <lhaury@gmail.com>
2020-01-05 13:34:24 -0800

    better description for the package

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='circum',
@@ -7,6 +14,8 @@ setup(
     author_email="lane@lumineerlabs.com",
     description="Circum is a set of tools for detecting and tracking moving objects" +
                 "via a variety of distributed sensors.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/LumineerLabs/circum",
     classifiers=[
         'License :: OSI Approved :: MIT License',
